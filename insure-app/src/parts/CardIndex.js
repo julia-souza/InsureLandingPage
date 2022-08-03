c20733ff53763027f298545e1c9769e1ce8d2c72
Júlia Souza <juliadsouzaborges@gmail.com>
2022-08-03 19:56:33 -0300

    fix

--- insure-app/src/parts/CardIndex.js
+++ insure-app/src/parts/CardIndex.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { useState, useEffect} from 'react';
+
 import "./css/CardIndex.css"
 
+const url = "http://localhost:3001/planos";
+
 const CardIndex = () => {
-  return (
-    <div id="planosView" class="planCard">
-        <div class="planCard-img"></div>
-    </div>
-  );
+    const [plano, setPlano] = useState([]);
+
+    useEffect(() => {
+        fetch('../../public/db.json',{
+            headers: {
+                Accept: 'application/json'
+            }
+        }).then(res => res.json())
+          .then(res =>setPlano(res.plano))
+    }, []);
+    
+    return <div id="planosView" class="planCar">
+        <div class="planCard-img" />
+        <div class="planCard-text">
+            <h1>{plano.nome}</h1>
+            <p>{plano.preco}</p>
+            <p>{plano.descricao}</p>	
+        </div>
+    </div>;
+  
 }
 
 export default CardIndex;
--- insure-app/src/parts/Nav.js
+++ insure-app/src/parts/Nav.js
@@ -7,7 +7,7 @@ const Nav = () => {
         <div class="logo"></div>
         <div class="nav-items"> 
             <ul>
-                <li> <a href='#'> Home </a> </li>
+                <li> <a href='/'> Home </a> </li>
                 <li> <a class="plano" href='#planosView'>Planos </a></li>
             </ul>
         </div>
